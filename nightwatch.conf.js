8e55e344870cae1ddfdf28b422c495dced836f83
MacBook <nahuelnucera990@gmail.com>
2019-02-02 16:17:13 +0100

    Set initial conifguration plus Travis CI

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -3,37 +3,39 @@ const seleniumServer = require("selenium-server");
 const chromedriver = require("chromedriver");
 
 module.exports = {
-    "src_folders": [
-      "test/e2e"// Where you are storing your Nightwatch e2e tests
-    ],
-    "output_folder": "./reports", // reports (test outcome) output by nightwatch
-    "selenium": {
-      "start_process": true, // tells nightwatch to start/stop the selenium process
-      "server_path": seleniumServer.path,
-      "host": "127.0.0.1",
-      "port": 4444, // standard selenium port
-      "cli_args": {
-        "webdriver.chrome.driver" : chromedriver.path
+  "src_folders": [
+    "test/e2e"
+  ],
+  "page_objects_path": ["test/po"],
+  "output_folder": "./reports",
+  "selenium": {
+    "start_process": true, // tells nightwatch to start/stop the selenium process
+    "server_path": seleniumServer.path,
+    "host": "127.0.0.1",
+    "port": 4444, // standard selenium port
+    "cli_args": {
+      "webdriver.chrome.driver": chromedriver.path
+    }
+  },
+  "test_settings": {
+    "default": {
+      "launch_url": "http://automationpractice.com",
+      // "screenshots": {
+      //   "enabled": true, // if you want to keep screenshots
+      //   "path": "./screen"
+      // },
+      "globals": {
+        "waitForConditionTimeout": 5000
+      },
+      "desiredCapabilities": { 
+        "browserName": "chrome"
       }
     },
-    "test_settings": {
-      "default": {
-        // "screenshots": {
-        //   "enabled": true, // if you want to keep screenshots
-        //   "path": SCREENSHOT_PATH // save screenshots here
-        // },
-        "globals": {
-          "waitForConditionTimeout": 5000 // sometimes internet is slow so wait.
-        },
-        "desiredCapabilities": { // use Chrome as the default browser for tests
-          "browserName": "chrome"
-        }
-      },
-      "chrome": {
-        "desiredCapabilities": {
-          "browserName": "chrome",
-          "javascriptEnabled": true // turn off to test progressive enhancement
-        }
+    "chrome": {
+      "desiredCapabilities": {
+        "browserName": "chrome",
+        "javascriptEnabled": true
       }
     }
   }
+}
